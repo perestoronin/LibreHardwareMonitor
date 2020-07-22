08364b4c9e3725ce62df6b3e8b3ffd943e12418a
v.perestoronin <v.perestoronin@russianpost.ru>
2020-07-23 02:36:01 +0300

    add demo get report for storage

---
+++ smartstorage/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+// using System.Text.Json;
+// using System.Text.Json.Serialization; //.JsonConverter;
+using LibreHardwareMonitor.Hardware;
+
+public class Program
+{
+    public class UpdateVisitor : IVisitor
+    {
+        public void VisitComputer(IComputer computer)
+        {
+            computer.Traverse(this);
+        }
+        public void VisitHardware(IHardware hardware)
+        {
+            hardware.Update();
+            foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+        }
+        public void VisitSensor(ISensor sensor) { }
+        public void VisitParameter(IParameter parameter) { }
+    }
+
+    static public void Main()
+    {
+        Computer computer = new Computer
+        {
+            IsCpuEnabled = false,
+            IsGpuEnabled = false,
+            IsMemoryEnabled = false,
+            IsMotherboardEnabled = false,
+            IsControllerEnabled = false,
+            IsNetworkEnabled = false,
+            IsStorageEnabled = true
+        };
+
+        computer.Open();
+        computer.Accept(new UpdateVisitor());
+/*
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+        };
+        Console.WriteLine(JsonSerializer.Serialize(computer.Hardware as IHardware, typeof(IHardware), options));
+*/
+        Console.WriteLine(computer.GetReport());
+    }
+}
